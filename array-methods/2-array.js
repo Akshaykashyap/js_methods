f458f5dfee88d099ff464606585c4290c7ab1674
Aman Sharma <sharmaman4419@gmail.com>
2019-06-25 18:34:47 +0530

    solve exercise

--- array-methods/2-array.js
+++ array-methods/2-array.js
@@ -8,6 +8,16 @@ var words = [
   'crackpot'
 ];
 //Write a function findLongestWord that takes an array of words and returns the longest one.
+//
+words.map(num => {
+  if(num>15) {
+      return num;
+  } else {
+      return 0;
+  }
+})
+//
+
 //If there are 2 with the same length, it should return the first occurrence.
 
 
@@ -23,9 +33,11 @@ var numbers1 = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 var numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 //Write a function averageNumbers that receives an array of numbers2 and calculate the average of the numbers
-
-
-
+numbers2.forEach(num => {
+  avg = num+num.length/2
+  console.log(sum);
+})
+//
 
 var words2 = [
   'seat',
@@ -41,5 +53,18 @@ var words2 = [
 ];
 //Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
 
-
+words2.forEach(num => {
+  avg = num+words.length/10
+  console.log(avg);
+})
+ seat0.7
+ correspond0.7
+ linen0.7
+ motif0.7
+ hole0.7
+  smell0.7
+ smart0.7
+ chaos0.7
+ fuel0.7
+ palace0.7
 
