c3fbe31a26e2a7d6e6580ea88094cfcb86cff2f6
Aman Sharma <sharmaman4419@gmail.com>
2019-06-25 21:14:07 +0530

    solve exercises

--- array-methods/practice.js
+++ array-methods/practice.js
@@ -14,13 +14,13 @@ var pizzas = [
 ];
 // map
 pizzas.map(value => value+" "+"dish")
-//
+//for each
 pizzas.forEach(value => {
   console.log(value+2)
 })
-//
+//filter
 pizzas.filter(value => value=length)
-//
+//splice
 pizzas.splice(1, 0, "fish");
 //
 
@@ -36,23 +36,23 @@ var cuts = [
   "Sirloin",
   "Round"
 ];
-//
+//map
 cuts.map(val => val+" "+"cuts")
-//
+//foreach
 cuts.forEach(val => val+"" 2)
-//
+//filter
 cuts.filter(val => val<cuts.length)
-//
+//splice
 cuts.splice(2,1,"hello")
 //
 
 var numbers = [1, 2, 3, 4, 5, 6, 23, 121, 345, 33, 23, 12, 435, 642, 66, 23];
-//
+// map
 numbers.map(function value(val){
   return val+2;
 }
-// 
+// filter
 numbers.filter(fun => fun<numbers.length)
-//
+//splice
 numbers.splice(2, 1, "welcome")
 
