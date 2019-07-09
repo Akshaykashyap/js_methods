2748729ab52b8f7c7dd692a94bd3bd5b49b0b84f
Aman Sharma <sharmaman4419@gmail.com>
2019-07-09 22:36:38 +0530

    completed object

--- array-methods/5-object.js
+++ array-methods/5-object.js
@@ -1,10 +1,12 @@
 // 1. Write a JavaScript program to list the properties and values of a JavaScript object. (Object.keys)
+
 let obj = {
   name: "aman" ,
   rollno: 10 ,
   batch: 8
 }
 // properties of the object 
+
 for (let key in obj) {
   console.log(key);
 }
@@ -12,6 +14,7 @@ for (let key in obj) {
  rollno
  batch
 // values
+
 for (let key in obj) {
   console.log(obj[key]);
 }
@@ -19,21 +22,26 @@ for (let key in obj) {
  10
  8
 // 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+
 var student = { 
   name : "David Rayy", 
   sclass : "VI", 
   rollno : 12
 };
 //before deleting
+
 student.rollno
 12
 //deleting student rollno
+
 delete student.rollno;
 true
 // after deleting
+
 student.rollno
 
 // 3. Write a function to get the length of an object.
+
 let obj = function (student) {
   return student;
 }
